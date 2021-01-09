6fabf055bcf83754e7e73ca55f8f73052388fbb4
Magno Vianna <mvianna@live.com>
2021-01-09 01:54:28 -0300

    Mapeamento objeto-relacional e Seed implementados

--- src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -5,18 +5,35 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address; 
 	private Double latitude; 
 	private Double longitude;
 	private Instant moment;
-	private Double total;
 	private OrderStatus status; 
 	
+	@ManyToMany
+	@JoinTable(name = "tb_order_product", 
+		joinColumns = @JoinColumn(name = "order_id"),
+		inverseJoinColumns = @JoinColumn(name = "product_id"))	
 	private Set<Product> products =  new HashSet<>(); 
 	
 	public Order() {
@@ -31,7 +48,6 @@ public class Order implements Serializable {
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
-		this.total = total;
 		this.status = status;
 	}
 
@@ -73,14 +89,6 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
-	}
-
-	public Double getTotal() {
-		return total;
-	}
-
-	public void setTotal(Double total) {
-		this.total = total;
 	}
 
 	public OrderStatus getStatus() {
